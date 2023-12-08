8a3b97703f35d668b75e2fa7a598826f00eb0ccb
mansoorbalouch <mansoorbaloch931@gmail.com>
2023-12-08 18:15:17 +0500

    modified:   Agent.m 	new file:   Run.asv 	modified:   Run.m 	new file:   estPrice.asv 	new file:   estPrice.m

--- Agent.m
+++ Agent.m
@@ -1,20 +1,28 @@
 classdef Agent
    properties
       agentID int64;
+      purpClass char;
       liquidity double;
-      holdings double;
-      holdingVol double;
-      type char;
+      % supply holdings and reserve volumes of n tokens
+      holdings(2,:) int64;
+      holdingVol(2,:) double;
+      stratType char;
+      risk double;
+      activity double;
    end
 
    methods
-       function obj = Agent(agentID, liquidity, holdings, holdingVol, type)
+       function obj = Agent(agentID,purpClass, liquidity, holdings, holdingVol, ...
+               stratType, risk, activity)
             if nargin > 0
                 obj.agentID = agentID;
+                obj.purpClass = purpClass;
                 obj.liquidity = liquidity;
                 obj.holdings = holdings;
                 obj.holdingVol = holdingVol;
-                obj.type = type;
+                obj.stratType = stratType;
+                obj.risk = risk;
+                obj.activity = activity;
             end
         end
        
